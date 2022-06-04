4b424556c8f48340fb3e3b659f20b3c4bc05993d
wolanski <kacpertomasz00@gmail.com>
2022-06-04 17:03:28 +0200

    I added drawing and changed functions places a little bit

--- main.py
+++ main.py
@@ -16,52 +16,75 @@ class Pixel:
         self.rect = pygame.Rect(x, y, self.length, self.height)
 
 
+
+
 class Screen:
     def __init__(self):
         self.draw_surface_length = Config.WINDOW_LENGTH * (Config.PIXEL_LENGTH - 1)
         self.draw_surface_height = Config.WINDOW_HEIGHT * (Config.PIXEL_HEIGHT - 1) + 1
         self.tool_menu_length = Config.SCREEN_LENGTH - self.draw_surface_length
         self.tool_menu_height = self.draw_surface_height
+        self.pixels = []
+        self.generate_pixels()
 
     def draw_the_window(self):
         WIN.fill(Colors.WHITE)
-        self.generate_pixels()
+        self.draw_the_drawing()
         self.generate_tool_menu()
         pygame.display.update()
 
     def generate_pixels(self):
+
         offset_x = 0
         offset_y = 0
 
+
         for x in range(Config.WINDOW_LENGTH):
             for y in range(Config.WINDOW_HEIGHT):
-                pixel = Pixel(offset_x, offset_y, Colors.RED)
-
-                pygame.draw.rect(WIN, pixel.color, pixel, 1)
-
+                self.pixels.append(Pixel(offset_x, offset_y, Colors.WHITE))
                 offset_y += Config.PIXEL_HEIGHT - 1
 
             offset_x += Config.PIXEL_LENGTH - 1
             offset_y = 0
 
-        # generate drawing space
+
+
+    def generate_tool_menu(self):
+
         pygame.draw.rect(WIN, Colors.LIGHT_GRAY,
                          pygame.Rect(0, 0, self.draw_surface_length, self.draw_surface_height), Config.PIXEL_LENGTH)
 
-    def generate_tool_menu(self):
+
         pygame.draw.rect(WIN, Colors.LIGHT_GRAY,
                          pygame.Rect(self.draw_surface_length, 0, self.tool_menu_length, self.tool_menu_height),
                          Config.PIXEL_LENGTH)
 
 
 
-    def find_pixel_color(self):
-        pass
+    def draw_the_drawing(self):
+        for i in self.pixels:
+
+            pygame.draw.rect(WIN, i.color, i)
+
+
+
+
+
+
+
+
+
+
+
+
 class Paint:
     def __init__(self):
         self.run = True
+        self.actual_color = Colors.BLACK
         self.screen = Screen()
+        self.drawing = False
         self.main_loop()
+
 
     def main_loop(self):
         while self.run:
@@ -69,10 +92,23 @@ class Paint:
             self.screen.draw_the_window()
             self.check_events()
 
+
     def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.drawing = True
+
+            elif event.type == pygame.MOUSEBUTTONUP:
+                self.drawing = False
+
+            if self.drawing:
+                for pixel in self.screen.pixels:
+                    if pixel.rect.collidepoint(pygame.mouse.get_pos()):
+                        pixel.color = Colors.BLUE
+
 
 
 def main():
